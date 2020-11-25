74bd4fde99b4eda2a4a0b792e77ad2b92b7e41f2
theBashShell <edohbruno@gmail.com>
2020-11-25 17:29:09 +0000

    footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,13 @@
+import Social from "../social/Social";
+
 export default function Footer() {
-  return <footer className="mt-auto">footer here</footer>;
+  return (
+    <footer className="mt-auto flex items-center justify-center flex-col">
+     
+      <div className="content block">
+        <Social />
+        <p>&copy;{new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
 }
--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -3,15 +3,15 @@ import style from "./navbar.module.css";
 export default function NavBar() {
   return (
     <nav
-      className={`fixed top-0 w-full z-10 ${style.nav} flex justify-center items-center`}
+      className={`fixed top-0 w-full z-10 ${style.nav} flex justify-center items-center md:px-5`}
     >
       <div
-        className={`flex justify-between items-center w-full h-full px-1 ${style.content}`}
+        className={`flex justify-between items-center w-full h-full px-3 content`}
       >
         <div>pic</div>
         <div className={`flex`}>
-          <div>dropdown</div>
-          <div>dropdown</div>
+          <div className="hover:underline hidden md:block">about</div>
+          <div className="md:hidden">menu</div>
         </div>
       </div>
     </nav>
